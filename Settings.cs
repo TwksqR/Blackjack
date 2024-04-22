7c5cef08762a81a8343c5e0fcb0c4cfa0c9441cc
Joshua <137341835+TwksqR@users.noreply.github.com>
2024-04-22 10:27:19 -0400

    Changed data type of field

--- Settings.cs
+++ Settings.cs
@@ -8,7 +8,7 @@ internal static class Settings
     internal static readonly int MinPlayers = 1;
     internal static readonly int MaxPlayers = 7;
 
-    internal static readonly decimal InitialWinnings = 100m;
+    internal static readonly int InitialWinnings = 100;
 
     internal static readonly int MinBet = 5;
     internal static readonly int MaxBet = 50;
