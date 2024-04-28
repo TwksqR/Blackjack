da5b4720e1a029896f67220cc024ded2ff2e2fb9
Joshua <137341835+TwksqR@users.noreply.github.com>
2024-04-28 14:38:36 -0400

    Commited

--- obj/Debug/net7.0/Blackjack.AssemblyInfo.cs
+++ obj/Debug/net7.0/Blackjack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5aeede994173ca7f3a8538478883fa8d2484c56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a72003bcf988715ad36e9fae0bf410d70dd491")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
