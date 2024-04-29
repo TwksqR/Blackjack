2526cc63e2be21c1afb0cfefb098355a979f6cc2
Joshua <137341835+TwksqR@users.noreply.github.com>
2024-04-28 23:54:15 -0400

    build: alpha 2.1.6.0
    MAJOR.MINOR.BUILD.REVISION
    
    MAJOR - Major changes to the program, incompatible with previous major versions
    
    MINOR - New feature that is generally backwards-compatible
    
    BUILD - Small changes and features
    
    REVISION - Generally hotfixes and other bugs

--- GameManager.cs
+++ GameManager.cs
@@ -12,7 +12,9 @@ public static class GameManager
         Console.CursorVisible = false;
 
         ConsoleUI.WriteColoredLine("Blackjack Simulator", ConsoleColor.Magenta);
-        ConsoleUI.WriteColoredLine("v0.1.0-alpha", ConsoleColor.DarkGray);
+        ConsoleUI.WriteColoredLine("Alpha 2.1.6.0", ConsoleColor.DarkGray);
+        ConsoleUI.WriteColoredLine("Release date: 2024.04.28", ConsoleColor.DarkGray);
+
         ConsoleUI.WriteColoredLine("\nMade by TwksqR\nDiscord: twskqr\nReddit: u/GD_Stalker", ConsoleColor.Cyan);
 
         ConsoleUI.DisplayButton("Let's play!");
--- obj/Debug/net7.0/Blackjack.AssemblyInfo.cs
+++ obj/Debug/net7.0/Blackjack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a9bfc8db9892f0da721971d590aaecd09073d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fb55f7e3fbe1d96f83fb58ef051a75181bcb05e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
