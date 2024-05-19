176d413e0d43cdc51b73ae9944701f0e1d8a8c5a
Joshua <137341835+TwksqR@users.noreply.github.com>
2024-05-19 14:50:27 -0400

    chore: update misc files

--- obj/Debug/net7.0/Blackjack.AssemblyInfo.cs
+++ obj/Debug/net7.0/Blackjack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.2.14.3")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.2.14.3+335a04ec26de9da42db8c971f83ebba3712b4eb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.2.14.3+3010fed3f4497b89b54b38de30575d46a8d12b9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.2.14.3")]
